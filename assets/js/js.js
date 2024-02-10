16d05bc6c8ffde786022bf463c7b26034b1be33b
arielsegumpan <arielsegumpan31@gmail.com>
2024-02-10 16:31:44 +0800

    update about page and gallery

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,37 +1,47 @@
-// Get all elements with class 'dropdown'
-var dropdowns = document.querySelectorAll('.dropdown');
+// // Get all elements with class 'dropdown'
+// var dropdowns = document.querySelectorAll('.dropdown');
 
-// Iterate over each dropdown element
-dropdowns.forEach(function(dropdown) {
-    // Add event listener for mouseenter event (hover in)
-    dropdown.addEventListener('mouseenter', function() {
-        // Find the dropdown menu within the current dropdown and fadeIn
-        var dropdownMenu = this.querySelector('.dropdown-menu');
-        if (dropdownMenu) {
-            dropdownMenu.style.display = 'block'; // Ensure menu is visible
-            setTimeout(function() {
-                dropdownMenu.style.opacity = '1'; // Fade in the menu
-            }, 10);
-        }
-    });
+// // Iterate over each dropdown element
+// dropdowns.forEach(function(dropdown) {
+//     // Add event listener for mouseenter event (hover in)
+//     dropdown.addEventListener('mouseenter', function() {
+//         // Find the dropdown menu within the current dropdown and fadeIn
+//         var dropdownMenu = this.querySelector('.dropdown-menu');
+//         if (dropdownMenu) {
+//             dropdownMenu.style.display = 'block'; // Ensure menu is visible
+//             setTimeout(function() {
+//                 dropdownMenu.style.opacity = '1'; // Fade in the menu
+//             }, 10);
+//         }
+//     });
 
-    // Add event listener for mouseleave event (hover out)
-    dropdown.addEventListener('mouseleave', function() {
-        // Find the dropdown menu within the current dropdown and fadeOut
-        var dropdownMenu = this.querySelector('.dropdown-menu');
-        if (dropdownMenu) {
-            dropdownMenu.style.opacity = '0'; // Fade out the menu
-            setTimeout(function() {
-                dropdownMenu.style.display = 'none'; // Hide menu after fading out
-            }, 300);
-        }
-    });
+//     // Add event listener for mouseleave event (hover out)
+//     dropdown.addEventListener('mouseleave', function() {
+//         // Find the dropdown menu within the current dropdown and fadeOut
+//         var dropdownMenu = this.querySelector('.dropdown-menu');
+//         if (dropdownMenu) {
+//             dropdownMenu.style.opacity = '0'; // Fade out the menu
+//             setTimeout(function() {
+//                 dropdownMenu.style.display = 'none'; // Hide menu after fading out
+//             }, 300);
+//         }
+//     });
 
-    // Add CSS transition for smooth appearance
-    dropdown.querySelector('.dropdown-menu').style.transition = 'opacity 0.3s ease';
-});
+//     // Add CSS transition for smooth appearance
+//     dropdown.querySelector('.dropdown-menu').style.transition = 'opacity 0.3s ease';
+// });
 
-
+ // Add hover event handlers
+ $('.dropdown').hover(
+  function () {
+      // On hover in
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(300);
+  },
+  function () {
+      // On hover out
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(300);
+  }
+);
 
 document.addEventListener('DOMContentLoaded', function () {
   const scrollBtn = document.getElementById('scroll_btn');
@@ -120,4 +130,44 @@ function handleIntersection(entries, observer) {
   });
   
 
-  
+  $(document).ready(() => {
+    $('[data-fancybox="gallery"]').fancybox({
+      buttons: [
+        "slideShow",
+        "thumbs",
+        "zoom",
+        "fullScreen",
+        "share",
+        "close"
+      ],
+      loop: false,
+      protect: true
+    });
+  });
+
+$(document).ready(function () {
+  let container = $('.container-img');
+  let cards = $('.card');
+
+  $('#gallery-search').on('input', function () {
+      let searchTerm = $(this).val().toLowerCase();
+      let matchingCards = cards.filter(function () {
+      let caption = $(this).find('a').data('caption').toLowerCase();
+          return caption.includes(searchTerm);
+      });
+
+      // Clear the container
+      container.empty();
+
+      // Append matching cards to the container with fadeIn animation
+      matchingCards.each(function () {
+          $(this).removeClass('fade-out').addClass('fade-in').appendTo(container).hide().fadeIn();
+      });
+
+      // Add fade-out class to non-matching cards and fade them out
+      cards.not(matchingCards).each(function () {
+          $(this).removeClass('fade-in').addClass('fade-out').fadeOut();
+      });
+  });
+});
+
